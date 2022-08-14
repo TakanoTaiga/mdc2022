d6ee6bde0863ffa4b18135b6a878599513ebd953
Taiga <53041471+TakanoTaiga@users.noreply.github.com>
2022-08-15 00:57:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
 #include "mbed.h"
-#include <cstdint>
-#include <cstdio>
-#include <string>
-#include <vector>
 
 #define MAXIMUM_BUFFER_SIZE 32
 
@@ -10,106 +6,53 @@
 // M:1:700:0:100:1:700:0:100:E
 // M:0:100:1:700:0:100:1:700:E
 
-int ctoi_ori(char c) {
-  switch (c) {
-  case '0':
-    return 0;
-    break;
-  case '1':
-    return 1;
-    break;
-  case '2':
-    return 2;
-    break;
-  case '3':
-    return 3;
-    break;
-  case '4':
-    return 4;
-    break;
-  case '5':
-    return 5;
-    break;
-  case '6':
-    return 6;
-    break;
-  case '7':
-    return 7;
-    break;
-  case '8':
-    return 8;
-    break;
-  case '9':
-    return 9;
-    break;
-
-  default:
-    return 0;
-    break;
-  }
-}
-
-double pwrCreater(char *data, int n, int index) {
-  char cut_data[16][16] = {{0}};
-  int cnt_index = 0;
-  int cnt_char = 0;
-  for (int i = 0; i < n; i++) {
-    if (data[i] == ':') {
-      cnt_index++;
-      cnt_char = 0;
-    } else {
-      cut_data[cnt_index][cnt_char] = data[i];
-      cnt_char++;
+int* pwrCreater(char *data) {
+    char cut_data[16][8] = {{0}};
+    int cnt_index = 0;
+    int cnt_char = 0;
+    for (int i = 0; data[i] != 'E' && cnt_index < 15; i++) {
+        if (data[i] == ':') {
+            cnt_index++;
+            cnt_char = 0;
+        } else {
+            cut_data[cnt_index][cnt_char] = data[i];
+            cnt_char++;
+        }
     }
-  }
-
-  size_t size = std::strlen(cut_data[index * 2 + 2]);
-  if (cut_data[index * 2 + 1][0] == '1') {
-    double rt = 0.0;
-
-    for (int x = 0; x < size; x++) {
-      rt += (double)(ctoi_ori(cut_data[index * 2 + 2][x]) *
-                     std::pow(10, (size - x - 1)));
+    
+    static int data_int[16] = {0};
+    for (int8_t i = 0; i < 14; i++) {
+        data_int[i] = std::atoi(cut_data[i + 1]);
     }
-    return rt / 1000;
-
-  } else if (cut_data[index * 2 + 1][0] == '0') {
-    double rt = 0.0;
-    for (int x = 0; x < size; x++) {
-      rt += (double)(ctoi_ori(cut_data[index * 2 + 2][x]) *
-                     std::pow(10, (size - x - 1)) * -1);
-    }
-    return rt / 1000;
-  }else{
-      return 0;
-  }
+    
+    return data_int;
 }
 
 static BufferedSerial serial_port(USBTX, USBRX);
 
 // pin setup
-PwmOut FR_pwm(PB_8);
-PwmOut FL_pwm(PB_9);
-PwmOut RR_pwm(PA_5);
-PwmOut RL_pwm(PA_6);
+PwmOut V1_PWM(PB_8);
+PwmOut V2_PWM(PB_9);
+PwmOut V3_PWM(PA_5);
+PwmOut V4_PWM(PA_6);
 
-DigitalOut FR_dgital(PC_9);
-DigitalOut FL_dgital(PC_8);
-DigitalOut RR_dgital(PC_6);
-DigitalOut RL_dgital(PC_5);
+DigitalOut V1_Digital(PC_9);
+DigitalOut V2_Digital(PC_8);
+DigitalOut V3_Digital(PC_6);
+DigitalOut V4_Digital(PC_5);
 
 int main() {
 
   // Serial
-  serial_port.set_baud(230400);
+  serial_port.set_baud(115200);
   serial_port.set_format(8, BufferedSerial::None, 1);
   char buf[MAXIMUM_BUFFER_SIZE] = {0};
 
   // PWM setup
-  FR_pwm.period_us(100);
-  FL_pwm.period_us(100);
-  RR_pwm.period_us(100);
-  RL_pwm.period_us(100);
+  V1_PWM.period_us(1000);
+  V2_PWM.period_us(1000);
+  V3_PWM.period_us(1000);
+  V4_PWM.period_us(1000);
 
   char pwrData[32] = {0};
   uint32_t pdConter = 0;
@@ -133,50 +76,39 @@ int main() {
           pdConter++;
         } else if (buf[i] == 'E') {
           pwrData[pdConter] = buf[i];
-          //serial_port.write(pwrData, sizeof(pwrData));
-          //char c = '\t';
-          //serial_port.write(&c, sizeof(c));
 
-          double motorPower[4] = {0.0};
-          motorPower[0] = pwrCreater(pwrData, sizeof(pwrData), 0);
-          motorPower[1] = pwrCreater(pwrData, sizeof(pwrData), 1);
-          motorPower[2] = pwrCreater(pwrData, sizeof(pwrData), 2);
-          motorPower[3] = pwrCreater(pwrData, sizeof(pwrData), 3);
+          int* motorPower = pwrCreater(pwrData);
 
-          //char debug[128] = {0};
-          //sprintf(debug, "Debug:0:%d:1:%d:2:%d:3:%d\n" , (int)(motorPower[0] * 1000.0), (int)(motorPower[1] * 1000.0), (int)(motorPower[2] * 1000.0), (int)(motorPower[3] * 1000.0));
-          //serial_port.write(&debug, sizeof(debug));
-
-          if (motorPower[0] >= 0.0) {
-            FR_pwm.write(motorPower[0]);
-            FR_dgital.write(1);
+          if (*motorPower == 1) {
+            V1_PWM.pulsewidth_us(*(motorPower + 1));
+            V1_Digital = 1;
           } else {
-            FR_pwm.write(motorPower[0] * -1);
-            FR_dgital.write(0);
+            V1_PWM.pulsewidth_us(*(motorPower + 1));
+            V1_Digital = 0;
           }
 
-          if (motorPower[1] >= 0.0) {
-            FL_pwm.write(motorPower[1]);
-            FL_dgital.write(1);
+          if (*(motorPower + 2) == 1) {
+            V2_PWM.pulsewidth_us(*(motorPower + 3));
+            V2_Digital = 1;
           } else {
-            FL_pwm.write(motorPower[1] * -1);
-            FL_dgital.write(0);
+            V2_PWM.pulsewidth_us(*(motorPower + 3));
+            V2_Digital = 0;
           }
 
-          if (motorPower[2] >= 0.0) {
-            RR_pwm.write(motorPower[2]);
-            RR_dgital.write(1);
+          if (*(motorPower + 4) == 1) {
+            V3_PWM.pulsewidth_us(*(motorPower + 5));
+            V3_Digital = 1;
           } else {
-            RR_pwm.write(motorPower[2] * -1);
-            RR_dgital.write(0);
+            V3_PWM.pulsewidth_us(*(motorPower + 5));
+            V3_Digital = 0;
           }
 
-          if (motorPower[3] >= 0.0) {
-            RL_pwm.write(motorPower[3]);
-            RL_dgital.write(1);
+          if (*(motorPower + 6) == 1) {
+            V4_PWM.pulsewidth_us(*(motorPower + 7));
+            V4_Digital = 1;
           } else {
-            RL_pwm.write(motorPower[3] * -1);
-            RL_dgital.write(0);
+            V4_PWM.pulsewidth_us(*(motorPower + 7));
+            V4_Digital = 0;
           }
 
           pdConter = 0;
